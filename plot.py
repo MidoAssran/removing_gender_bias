a2d1f70d161ab945f677e398636bdb1ca3416693
choij <jonathan.choi@mail.mcgill.ca>
2017-04-16 04:39:15 -0400

    ratio varying with size n

--- plot.py
+++ plot.py
@@ -16,11 +16,12 @@ def mf_ratio(method,gender,n):
       data_debiased = []
 
       for line1,line2 in zip(res1,res2):
-        data_biased.append( Counter(line1[:n]) )
-        data_debiased.append( Counter(line2[:n]) )   
+        data_biased.append( Counter(line1) )
+        data_debiased.append( Counter(line2) )
 
       ratios   = []
       ratios_d = []
+
       for i in range (len(data_debiased)):
         male     = data_biased[i]["male"]
         male_d   = data_debiased[i]["male"]
@@ -30,8 +31,42 @@ def mf_ratio(method,gender,n):
         ratios.append( male/(female+male) )
         ratios_d.append( male_d/(female_d+male_d) )
  
-      return ratios, ratios_d
+      #print (np.std(ratios))
+      #print (np.std(ratios_d))
+      #print (ratios, ratios_d)
+      return np.average(ratios), np.average(ratios_d)
 
-print ( mf_ratio("cosine", 1, 100) )
-print ( mf_ratio("jaccard", 1, 100) )
-print ( mf_ratio("euclidean", 1, 100) )
+def get_ratios(n):
+  print ("n : ",n)
+  print ("cosine : ", mf_ratio("cosine", 1, n) )
+  print ( "jaccard : ", mf_ratio("jaccard", 1, n) )
+  print ( "euclidean : ", mf_ratio("euclidean", 1, n) )
+
+def plot(method):
+  n = np.arange(1,101)*100
+  r = [mf_ratio(method, 1, j) for j in n]
+
+  biased = np.array([x[0] for x in r])*100
+  debiased = np.array([x[1] for x in r])*100
+
+  biased_female = np.array([ 100-x for x in biased ])
+  debiased_female = np.array([100-x for x in debiased ])
+  print (r)
+  
+  m_b, = plt.plot(n,biased, 'b--')
+  m_d, = plt.plot(n,debiased, 'c--')
+  f_b, = plt.plot(n,biased_female, 'r--')
+  f_d, = plt.plot(n,debiased_female, 'm--')
+
+  plt.legend([m_b, m_d, f_b, f_d],['Male', 'Male Debiased', 'Female', 'Female Debiased'], loc="upper right", ncol=1, fontsize=8)
+  plt.title('Gender Percentage of the Top n Selected Candidates Using Cosine')
+  plt.ylabel('Percentage')
+  plt.xlabel('n')
+  plt.show()
+
+#def histogram():
+  
+if __name__ == "__main__":
+  #n = 3000
+  #get_ratios(n)
+  plot ("cosine")
